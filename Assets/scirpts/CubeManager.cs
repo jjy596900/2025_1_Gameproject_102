765e2be93ad848b0ea0ce36f8f7f2e40747ef40a
jjy596900 <twojsm05@gmail.com>
2025-04-21 14:01:03 +0900

    [Feat] if문 for문 배열을 활용해서 Tile 생성

---
+++ Assets/scirpts/CubeManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeManager : MonoBehaviour
+{
+    public CubeGenerator[] generatedCubes = new CubeGenerator[5];
+
+    public float timer = 0.0f;
+    public float interval = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= interval)
+        {
+            RandomizeCubeActivaltion();
+            timer = 0.0f;
+        }
+    }
+
+    public void RandomizeCubeActivaltion() 
+    {
+        for (int i = 0; i < generatedCubes.Length; i++)
+        {
+            int randomNum = Random.Range(0, 2);
+            if (randomNum == 1) 
+            {
+                generatedCubes[i].GenCube();
+            }
+        }
+    }
+
+
+}
